94889fa62ed905d36c3e74ae89ab86aee083c06f
Franklin Bado <fbado66@gmail.com>
2020-04-04 20:40:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
     result = []
       array.each do |i|
-    result.push ( i ** 2)
-    # result << (i ** 2)
+    # result.push ( i ** 2)
+     result << (i ** 2)
   end
   result
 
